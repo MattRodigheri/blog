db26467a43e8c0d88e07b144405b345a34af484e
MattRodigheri <mrodigheri@gmail.com>
2019-10-10 14:16:34 -0700

    bug fixes

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 3001;
 app.use(favicon(__dirname + "/build/favicon.ico"));
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
 
-// app.get("/*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
