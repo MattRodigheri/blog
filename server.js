7911e948316c75bca85237752c04fdad5749fb54
MattRodigheri <mrodigheri@gmail.com>
2019-10-10 15:26:51 -0700

    build

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(favicon(__dirname + "/build/favicon.ico"));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("/*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
